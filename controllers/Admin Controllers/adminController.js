b5f78d12450d5646daadf29aa3e7c86c05da1dee
usama7365 <usamaaamirsohail@gmail.com>
2023-09-28 03:34:16 -0700

    resolve bug on login page

--- controllers/Admin Controllers/adminController.js
+++ controllers/Admin Controllers/adminController.js
@@ -98,6 +98,7 @@ exports.AdminLogin = async (req, res) => {
       message: 'Login successful',
       role: admin.role,
       name: admin.name,
+      email: admin.email,
       token,
       isVerified: admin.isVerified,
       createdAt: admin.createdAt,
