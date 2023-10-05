1f8cc5b6629937582f7a1372916720e1d7f51dd8
usama7365 <usamaaamirsohail@gmail.com>
2023-10-05 10:35:58 -0700

    fix display images

--- routes/image.route.js
+++ routes/image.route.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/uploads/images');
+    cb(null, 'public/uploads/images');
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
